492d0fe24865c9698a078faa7255777767d987c7
Robert Xiu <xiurobert@gmail.com>
2015-05-02 09:37:43 +0800

    Create Travis yml

---
+++ src/main/java/net/lightningsf/skBomb/logging/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Created by xiurobert on 5/2/2015.
+ */
+package net.lightningsf.skBomb.logging;
+
+/**
+ * This package contains the error logging utils for skBomb
+ */
---
+++ src/main/java/net/lightningsf/skBomb/main.java
@@ -0,0 +1,12 @@
+package net.lightningsf.skBomb;
+
+/**
+ * Created by xiurobert on 5/2/2015.
+ */
+
+import static java.lang.System.out;
+public class main {
+    public static void main(String[] args) {
+        out.println("Loading skBomb Community v1");
+    }
+}
